14d649349fd28e1766f52601c7624cff09aa5a8f
guxingke <admin@guxingke.com>
2021-01-11 12:29:35 +0800

    :tada: init from 0.99

---
+++ include/config-int.h
@@ -0,0 +1,9 @@
+#ifndef _CLASSPATH_INCLUDE_CONFIG_INT_H
+#define _CLASSPATH_INCLUDE_CONFIG_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "classpath 0.99"
+/* generated using gnu compiler Apple clang version 11.0.0 (clang-1100.0.33.16) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
